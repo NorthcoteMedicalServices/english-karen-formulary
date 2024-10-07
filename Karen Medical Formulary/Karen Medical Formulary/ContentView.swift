8dee6c06557d371780064f0c1de29d4d9f4629f4
Charlie Havener <havenec@wwu.edu>
2024-10-07 16:19:40 -0700

    iOS App Project Setup

---
+++ Karen Medical Formulary/Karen Medical Formulary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Karen Medical Formulary
+//
+//  Created by Charlie Havener on 10/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KarenMedicalFormulary/Karen Medical Formulary/Karen_Medical_FormularyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Karen_Medical_FormularyApp.swift
+//  Karen Medical Formulary
+//
+//  Created by Charlie Havener on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Karen_Medical_FormularyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
