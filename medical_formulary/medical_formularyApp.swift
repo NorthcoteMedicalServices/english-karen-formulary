45eec3672a68c85d228befcda618e924049af3b1
lowneyp <lowneyp@wwu.edu>
2024-10-09 14:25:51 -0700

    Initial project commit

---
+++ medical_formulary/medical_formularyApp.swift
@@ -0,0 +1,17 @@
+//
+//  medical_formularyApp.swift
+//  medical_formulary
+//
+//  Created by Parker Lowney on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct medical_formularyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
