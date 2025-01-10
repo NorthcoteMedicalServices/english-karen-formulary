c91aba13d2c92c28a3f6a5547b2ec201f32e92af
havenec <havenec@wwu.edu>
2025-01-09 17:23:41 -0800

    translation setup for navigation

--- translate.js
+++ translate.js
@@ -30,34 +30,36 @@ function translate(){
 // the english translation at index 0 and Karen translation at index 1
 translations = {
   //Home Page
-  home1:["DRUG SEARCH","karen drugs"],
-  home2:["Z SCORES","karen z"],
-  home3:["VIEW FORMULARY","karen view"],
-  home4:["DRUGS IN PREGNANCY","karen prego"],
+  home1:["DRUG SEARCH","တၢ်ဃုကသံၣ်"],
+  home2:["Z-SCORES","Z-SCORES"],
+  home3:["VIEW FORMULARY","တၢ်ကွၢ်ကသံၣ်လံာ်နဲၣ်ကျဲအံၤ"],
+  home4:["DRUGS IN PREGNANCY","ကသံၣ်သ့ၣ်တဖၣ်လၢၦၤမိၢ်ဒၢဂီၢ်"],
   //Drug Search
 
-  //Z Scores
-
   // Table of contents
-  tc1:["Essential Drug Formulary 2021 edition","karenTest1"],
-  tc2:["Introduction To 2021 Edition","karenTest2"],
-  tc3:["Introduction To 2013 Edition","karenTest3"],
-  tc4:["Genereal Guidelines On How To Use The Formulary","karenTest4"],
-  tc5:["Drugs In Pregnancy","karenTest5"],
-  tc6:["Drugs and G6PD Deficiency","karenTest6"],
-  tc7:["Drugs List By Group","karenTest7"],
-  tc8:["About Ferros (Iron), Vitamins and Multivitamins","karenTest8"],
-  tc9:["Combination Treatment For Genito-Urinary Diseases","karenTest9"],
-  tc10:["Drugs Used For Post Partum Haemorrage (PPH) And Missed Or Incomplete Abortion",""],
-  tc11:["About Anti-Retroviral Drugs",""],
-  tc12:["Use Of Antibiotics In Combination",""],
-  tc13:["Drugs For The Treatement of New Cases Of TB",""],
-  tc14:["Antiseptics - Disinfectants",""],
-  tc15:["Wound Care / Abscess Care",""],
-  tc16:["IV Fluids",""],
-  tc17:["Malaria Protocols",""],
-  tc18:["Vaccination Schedule",""]
-
-  //Drugs in Pregnancy
+  tc1:["Essential Drug Formulary 2021 edition","ကသံၣ်လံာ်နဲၣ်ကျဲ"],
+  tc2:["Introduction To 2021 Edition","၂၀၂၁ ကသံၣ်လံာ်နဲၣ်ကျဲ"],
+  tc3:["Introduction To 2013 Edition","၂၀၁၃ ကသံၣ်လံာ်နဲၣ်ကျဲ"],
+  tc4:["Genereal Guidelines On How To Use The Formulary","တၢ်သိၣ်တၢ်သီလၢတၢ်ကဘၣ်သူလံာ်တၢ်နဲၣ်ကျဲအံၤဒ်လဲၣ်"],
+  tc5:["Drugs In Pregnancy","ကသံၣ်သ့ၣ်တဖၣ်လၢၦၤမိၢ်ဒၢဂီၢ်"],
+  tc6:["Drugs and G6PD Deficiency","ကသံၣ်ဒီး(ကၠံၤစံၣ်ဖံၤဒံၤ)လီၤထုးတဖၣ်"],
+  tc7:["Drugs List By Group","ကသံၣ်တဖၣ်ဘၣ်တၢ်ရဲၣ်လီၤအီၤလၢအကဂူၢ်"],
+  tc8:["About Ferros (Iron), Vitamins and Multivitamins","ဘၣ်ဃးဒီးထး(Ferrous/Iron) ၀ံၤတၤမံၤဒီး၀ဲၤတၤမံၤပာ်ဖှိၣ်တဖၣ်"],
+  tc9:["Combination Treatment For Genito-Urinary Diseases","တၢ်ကူဆါယါဘျါကသံၣ်လၢဘၣ်တၢ်ပာ်ဖှိၣ်ဃုာ်အီၤလၢပိာ်မုၣ်ပိာ်ခွါက့ၢ်ဂီၤတၢ်ဆါတဖၣ်"],
+  tc10:["Drugs Used For Post Partum Haemorrage (PPH) And Missed Or Incomplete Abortion","ကသံၣ်လၢတၢ်သူအီၤလၢအိၣ်ဖျဲၣ်၀ံၤသွံၣ်လူဒီးဖိသၣ်ဟးဂီၤလၢဒၢလီၢ်ပူၤ(ဟုးဟးဂီၤ) မ့ မိၢ်ဒၢဟုးလီၤဖှံၣ်တ၀ာ်"],
+  tc11:["About Anti-Retroviral Drugs","ဘၣ်ဃးဒီး(အဲၤထံၤရံၤထြိၤ၀ံၤရၤ)ကသံၣ်တဖၣ"],
+  tc12:["Use Of Antibiotics In Combination","တၢ်သူ၀ဲကသံၣ်မၤသံ (ဘၣ်ထံရံယၤ) ဃၢ်တဖၣ်လၢအဘၣ်တၢ်ပာ်ဖှိၣ်ဃုာ်အီၤ"],
+  tc13:["Drugs For The Treatement of New Cases Of TB","ကသံၣ်ကူစါယါဘျါ(ထံၣ်ဘံၣ်)ဃၢ်လၢၦၤလၢအဘၣ်တၢ်ဆါအသီတဖၣ်အဂီၢ်"],
+  tc14:["Antiseptics - Disinfectants","ကသံၣ်ထံမၤသံ(ဘၣ်ထံရံယၤ)ဒီး(၀ံၤရူစ်)ဃၢ်လၢဖုဒါခိၣ်ဒီးတၢ်ပီးတၢ်လီအကလုာ်ကလုာ်"],
+  tc15:["Wound Care / Abscess Care","တၢ်ကွၢ်ထွဲတၢ်ပူၤလီၢ်၊တၢ်ကွၢ်ထွဲ တၢ်အဖံ(၀့ထီၣ်) တဖၣ်"],
+  tc16:["IV Fluids","ကသံၣ်လၢဘၣ်သွီလၢသွံၣ်ကျိၤ (အံသၣ်ထံတဖၣ်)"],
+  tc17:["Malaria Protocols","တၢ်ညၣ်ဂိၢ်ဃၢ်ကသံၣ်အကျိၤအကျဲ"],
+  tc18:["Vaccination Schedule","ကသံၣ်ဒီသဒၢအတၢ်ရဲၣ်တၢ်ကျဲၤ (ယိၤကီၢ် ၊ ပယီၤကီၢ်)"]
 
 };
+
+/*
+        android:text="DRUGS IN PREGNANCY\nကသံၣ်သ့ၣ်တဖၣ်လၢၦၤမိၢ်ဒၢဂီၢ်"
+        android:text="View FORMULARY\nတၢ်ကွၢ်ကသံၣ်လံာ်နဲၣ်ကျဲအံၤ"
+        android:text="Drug Search\nတၢ်ဃုကသံၣ်"
+*/
